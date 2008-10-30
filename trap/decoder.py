1252f84964a4e6dc5445fd25c6db2dc897d06e6d
fossati.l <fossati.l@b5a49c6b-0751-0410-b22d-d588f1dd31d4>
2008-10-30 17:03:29 +0000

    slightly improved decoder generation

--- trap/decoder.py
+++ trap/decoder.py
@@ -320,9 +320,12 @@ class decoderCreator:
         outEdges = self.decodingTree.out_edges(subtree)
         mask = subtree.splitFunction.toCode()
         outEdges = sorted(outEdges, lambda x, y: cmp(y[-1][-1], x[-1][-1]))
-        code = 'switch(instrCode & ' + mask + '){\n'
+        if '0' in  mask[2:]:
+            code = 'switch(instrCode & ' + mask + '){\n'
+        else:
+            code = 'switch(instrCode){\n'
         for edge in outEdges:
-            code += 'case ' + hex(edge[-1][0]) + ':\n'
+            code += 'case ' + hex(edge[-1][0]) + ':{\n'
             if edge[1].instrId != None:
                 if edge[1].instrId != -1:
                     code += '// Instruction ' + self.instrName[edge[1].instrId] + '\nreturn ' + str(edge[1].instrId) + ';\n'
@@ -332,8 +335,8 @@ class decoderCreator:
                 code += self.createPatternDecoder(edge[1])
             else:
                 code += self.createTableDecoder(edge[1])
-            code += 'break;\n'
-        code += 'default:\nTHROW_EXCEPTION(\"Fatal Error, pattern \" << std::hex << std::showbase << instrCode << std::dec << \" not recognized by the decoder\");\n'
+            code += 'break;}\n'
+        code += 'default:{\nTHROW_EXCEPTION(\"Fatal Error, pattern \" << std::hex << std::showbase << instrCode << std::dec << \" not recognized by the decoder\");}\n'
         code += '}\n'
         return code
 
