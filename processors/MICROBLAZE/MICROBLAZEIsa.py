d918be9973e25ca138159f0b67f91a17bd62f2a1
yans86 <yans86@b5a49c6b-0751-0410-b22d-d588f1dd31d4>
2009-04-11 21:35:37 +0000

    * The structure of each instruction is now COMPLETE! * some bug fixes

--- processors/MICROBLAZE/MICROBLAZEIsa.py
+++ processors/MICROBLAZE/MICROBLAZEIsa.py
@@ -211,7 +211,7 @@ isa.addInstruction(and_Instr)
 opCode = cxx_writer.writer_code.Code("""
 
 """)
-andi_Instr = trap.Instruction('AND', True)
+andi_Instr = trap.Instruction('ANDI', True)
 andi_Instr.setMachineCode(oper_imm, {'opcode': [1,0,1,0,0,1]}, 'TODO')
 andi_Instr.setCode(opCode,'execute')
 isa.addInstruction(andi_Instr)
@@ -593,7 +593,7 @@ isa.addInstruction(brki_Instr)
 #BSRL (S=0, T=0)
 opCode = cxx_writer.writer_code.Code("""
 
-""")­
+""") 
 bsrl_Instr = trap.Instruction('BSRL', True)
 bsrl_Instr.setMachineCode(barrel_reg, {'opcode0': [0,1,0,0,0,1], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
 bsrl_Instr.setCode(opCode,'execute')
@@ -602,7 +602,7 @@ isa.addInstruction(bsrl_Instr)
 #BSRA (S=0, T=1)
 opCode = cxx_writer.writer_code.Code("""
 
-""")­
+""")
 bsra_Instr = trap.Instruction('BSRA', True)
 bsra_Instr.setMachineCode(barrel_reg, {'opcode0': [0,1,0,0,0,1], 'opcode1': [0,1,0,0,0,0,0,0,0,0,0]}, 'TODO')
 bsra_Instr.setCode(opCode,'execute')
@@ -611,7 +611,7 @@ isa.addInstruction(bsra_Instr)
 #BSLL (S=1, T=0)
 opCode = cxx_writer.writer_code.Code("""
 
-""")­
+""")
 bsll_Instr = trap.Instruction('BSLL', True)
 bsll_Instr.setMachineCode(barrel_reg, {'opcode0': [0,1,0,0,0,1], 'opcode1': [1,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
 bsll_Instr.setCode(opCode,'execute')
@@ -620,7 +620,7 @@ isa.addInstruction(bsll_Instr)
 #BSRLI (S=0, T=0)
 opCode = cxx_writer.writer_code.Code("""
 
-""")­
+""")
 bsrli_Instr = trap.Instruction('BSRLI', True)
 bsrli_Instr.setMachineCode(barrel_imm, {'opcode0': [0,1,1,0,0,1], 'opcode1': [0,0,0,0,0], 'opcode2': [0,0,0,0,0,0]}, 'TODO')
 bsrli_Instr.setCode(opCode,'execute')
@@ -629,7 +629,7 @@ isa.addInstruction(bsrli_Instr)
 #BSRAI (S=0, T=1)
 opCode = cxx_writer.writer_code.Code("""
 
-""")­
+""")
 bsrai_Instr = trap.Instruction('BSRAI', True)
 bsrai_Instr.setMachineCode(barrel_imm, {'opcode0': [0,1,1,0,0,1], 'opcode1': [0,0,0,0,0], 'opcode2': [0,1,0,0,0,0]}, 'TODO')
 bsrai_Instr.setCode(opCode,'execute')
@@ -638,10 +638,561 @@ isa.addInstruction(bsrai_Instr)
 #BSLLI (S=0, T=1)
 opCode = cxx_writer.writer_code.Code("""
 
-""")­
+""")
 bslli_Instr = trap.Instruction('BSLLI', True)
 bslli_Instr.setMachineCode(barrel_imm, {'opcode0': [0,1,1,0,0,1], 'opcode1': [0,0,0,0,0], 'opcode2': [1,0,0,0,0,0]}, 'TODO')
 bslli_Instr.setCode(opCode,'execute')
 isa.addInstruction(bslli_Instr)
 
-#CMP ...
+#COMPARE family
+#CMP
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+cmp_Instr = trap.Instruction('CMP', True)
+cmp_Instr.setMachineCode(oper_reg, {'opcode0': [0,0,0,1,0,1], 'opcode1': [0,0,0,0,0,0,0,0,0,0,1]}, 'TODO')
+cmp_Instr.setCode(opCode,'execute')
+isa.addInstruction(cmp_Instr)
+
+#CMPU
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+cmpu_Instr = trap.Instruction('CMPU', True)
+cmpu_Instr.setMachineCode(oper_reg, {'opcode0': [0,0,0,1,0,1], 'opcode1': [0,0,0,0,0,0,0,0,0,1,1]}, 'TODO')
+cmpu_Instr.setCode(opCode,'execute')
+isa.addInstruction(cmpu_Instr)
+
+#FLOAT family
+#FADD
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+fadd_Instr = trap.Instruction('FADD', True)
+fadd_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,1,1,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+fadd_Instr.setCode(opCode,'execute')
+isa.addInstruction(fadd_Instr)
+
+#FRSUB
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+frsub_Instr = trap.Instruction('FRSUB', True)
+frsub_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,1,1,0], 'opcode1': [0,0,0,1,0,0,0,0,0,0,0]}, 'TODO')
+frsub_Instr.setCode(opCode,'execute')
+isa.addInstruction(frsub_Instr)
+
+#FMUL
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+fmul_Instr = trap.Instruction('FMUL', True)
+fmul_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,1,1,0], 'opcode1': [0,0,1,0,0,0,0,0,0,0,0]}, 'TODO')
+fmul_Instr.setCode(opCode,'execute')
+isa.addInstruction(fmul_Instr)
+
+#FDIV
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+fdiv_Instr = trap.Instruction('FDIV', True)
+fdiv_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,1,1,0], 'opcode1': [0,0,1,1,0,0,0,0,0,0,0]}, 'TODO')
+fdiv_Instr.setCode(opCode,'execute')
+isa.addInstruction(fdiv_Instr)
+
+#FCMP
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+fcmp_Instr = trap.Instruction('FCMP', True)
+fcmp_Instr.setMachineCode(float_cmp, {'opcode0': [0,1,0,1,1,0], 'opcode1': [0,1,0,0], 'opcode2': [0,0,0,0]}, 'TODO')
+fcmp_Instr.setCode(opCode,'execute')
+isa.addInstruction(fcmp_Instr)
+
+#FLT
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+flt_Instr = trap.Instruction('FLT', True)
+flt_Instr.setMachineCode(float_unary, {'opcode0': [0,1,0,1,1,0], 'opcode1': [0,1,0,1,0,0,0,0,0,0,0]}, 'TODO')
+flt_Instr.setCode(opCode,'execute')
+isa.addInstruction(flt_Instr)
+
+#FINT
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+fint_Instr = trap.Instruction('FINT', True)
+fint_Instr.setMachineCode(float_unary, {'opcode0': [0,1,0,1,1,0], 'opcode1': [0,1,1,0,0,0,0,0,0,0,0]}, 'TODO')
+fint_Instr.setCode(opCode,'execute')
+isa.addInstruction(fint_Instr)
+
+#FSQRT
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+fsqrt_Instr = trap.Instruction('FSQRT', True)
+fsqrt_Instr.setMachineCode(float_unary, {'opcode0': [0,1,0,1,1,0], 'opcode1': [0,1,1,1,0,0,0,0,0,0,0]}, 'TODO')
+fsqrt_Instr.setCode(opCode,'execute')
+isa.addInstruction(fsqrt_Instr)
+
+#GET / GETD
+#very strange instructions :)
+
+#IDIV
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+idiv_Instr = trap.Instruction('IDIV', True)
+idiv_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,0,1,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+idiv_Instr.setCode(opCode,'execute')
+isa.addInstruction(idiv_Instr)
+
+#IDIVU
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+idivu_Instr = trap.Instruction('IDIVU', True)
+idivu_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,0,1,0], 'opcode1': [0,0,0,0,0,0,0,0,0,1,0]}, 'TODO')
+idivu_Instr.setCode(opCode,'execute')
+isa.addInstruction(idivu_Instr)
+
+#IMM
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+imm_Instr = trap.Instruction('IMM', True)
+imm_Instr.setMachineCode(imm_code, {'opcode': [1,0,1,1,0,0]}, 'TODO')
+imm_Instr.setCode(opCode,'execute')
+isa.addInstruction(imm_Instr)
+
+#LOAD family
+#LBU
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+lbu_Instr = trap.Instruction('LBU', True)
+lbu_Instr.setMachineCode(oper_reg, {'opcode0': [1,1,0,0,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+lbu_Instr.setCode(opCode,'execute')
+isa.addInstruction(lbu_Instr)
+
+#LBUI
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+lbui_Instr = trap.Instruction('LBUI', True)
+lbui_Instr.setMachineCode(oper_imm, {'opcode': [1,1,1,0,0,0]}, 'TODO')
+lbui_Instr.setCode(opCode,'execute')
+isa.addInstruction(lbui_Instr)
+
+#LHU
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+lhu_Instr = trap.Instruction('LHU', True)
+lhu_Instr.setMachineCode(oper_reg, {'opcode0': [1,1,0,0,0,1], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+lhu_Instr.setCode(opCode,'execute')
+isa.addInstruction(lhu_Instr)
+
+#LHUI
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+lhui_Instr = trap.Instruction('LHUI', True)
+lhui_Instr.setMachineCode(oper_imm, {'opcode': [1,1,1,0,0,1]}, 'TODO')
+lhui_Instr.setCode(opCode,'execute')
+isa.addInstruction(lhui_Instr)
+
+#LW
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+lw_Instr = trap.Instruction('LW', True)
+lw_Instr.setMachineCode(oper_reg, {'opcode0': [1,1,0,0,1,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+lw_Instr.setCode(opCode,'execute')
+isa.addInstruction(lw_Instr)
+
+#LWI
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+lwi_Instr = trap.Instruction('LWI', True)
+lwi_Instr.setMachineCode(oper_imm, {'opcode': [1,1,1,0,1,0]}, 'TODO')
+lwi_Instr.setCode(opCode,'execute')
+isa.addInstruction(lwi_Instr)
+
+#MFS
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+mfs_Instr = trap.Instruction('MFS', True)
+mfs_Instr.setMachineCode(mfs_code, {'opcode': [1,0,0,1,0,1]}, 'TODO')
+mfs_Instr.setCode(opCode,'execute')
+isa.addInstruction(mfs_Instr)
+
+#MSRCLR
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+msrclr_Instr = trap.Instruction('MSRCLR', True)
+msrclr_Instr.setMachineCode(msr_oper, {'opcode0': [1,0,0,1,0,1], 'opcode1': [0,0,0,0,1,0]}, 'TODO')
+msrclr_Instr.setCode(opCode,'execute')
+isa.addInstruction(msrclr_Instr)
+
+#MSRSET
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+msrset_Instr = trap.Instruction('MSRSET', True)
+msrset_Instr.setMachineCode(msr_oper, {'opcode0': [1,0,0,1,0,1], 'opcode1': [0,0,0,0,0,0]}, 'TODO')
+msrset_Instr.setCode(opCode,'execute')
+isa.addInstruction(msrset_Instr)
+
+#MTS
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+mts_Instr = trap.Instruction('MTS', True)
+mts_Instr.setMachineCode(mts_code, {'opcode': [1,0,0,1,0,1]}, 'TODO')
+mts_Instr.setCode(opCode,'execute')
+isa.addInstruction(mts_Instr)
+
+#MUL
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+mul_Instr = trap.Instruction('MUL', True)
+mul_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,0,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+mul_Instr.setCode(opCode,'execute')
+isa.addInstruction(mul_Instr)
+
+#MULH
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+mulh_Instr = trap.Instruction('MULH', True)
+mulh_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,0,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,1]}, 'TODO')
+mulh_Instr.setCode(opCode,'execute')
+isa.addInstruction(mulh_Instr)
+
+#MULHU
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+mulhu_Instr = trap.Instruction('MULHU', True)
+mulhu_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,0,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,1,1]}, 'TODO')
+mulhu_Instr.setCode(opCode,'execute')
+isa.addInstruction(mulhu_Instr)
+
+#MULHSU
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+mulhsu_Instr = trap.Instruction('MULHSU', True)
+mulhsu_Instr.setMachineCode(oper_reg, {'opcode0': [0,1,0,0,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,1,0]}, 'TODO')
+mulhsu_Instr.setCode(opCode,'execute')
+isa.addInstruction(mulhsu_Instr)
+
+#MULI
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+muli_Instr = trap.Instruction('MULI', True)
+muli_Instr.setMachineCode(oper_imm, {'opcode': [0,1,1,0,0,0]}, 'TODO')
+muli_Instr.setCode(opCode,'execute')
+isa.addInstruction(muli_Instr)
+
+#OR
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+or_Instr = trap.Instruction('OR', True)
+or_Instr.setMachineCode(oper_reg, {'opcode0': [1,0,0,0,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+or_Instr.setCode(opCode,'execute')
+isa.addInstruction(or_Instr)
+
+#ORI
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+ori_Instr = trap.Instruction('ORI', True)
+ori_Instr.setMachineCode(oper_imm, {'opcode': [1,0,1,0,0,0]}, 'TODO')
+ori_Instr.setCode(opCode,'execute')
+isa.addInstruction(ori_Instr)
+
+#PCMPBF
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+pcmpbf_Instr = trap.Instruction('PCMPBF', True)
+pcmpbf_Instr.setMachineCode(oper_reg, {'opcode0': [1,0,0,0,0,0], 'opcode1': [1,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+pcmpbf_Instr.setCode(opCode,'execute')
+isa.addInstruction(pcmpbf_Instr)
+
+#PCMPEQ
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+pcmpeq_Instr = trap.Instruction('PCMPEQ', True)
+pcmpeq_Instr.setMachineCode(oper_reg, {'opcode0': [1,0,0,0,1,0], 'opcode1': [1,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+pcmpeq_Instr.setCode(opCode,'execute')
+isa.addInstruction(pcmpeq_Instr)
+
+#PCMPNE
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+pcmpne_Instr = trap.Instruction('PCMPNE', True)
+pcmpne_Instr.setMachineCode(oper_reg, {'opcode0': [1,0,0,0,1,1], 'opcode1': [1,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+pcmpne_Instr.setCode(opCode,'execute')
+isa.addInstruction(pcmpne_Instr)
+
+#PUT / PUTD
+#very strange instructions :)
+
+#RSUB instruction family
+#RSUB
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rsub_Instr = trap.Instruction('RSUB', True)
+rsub_Instr.setMachineCode(oper_reg, {'opcode0': [0,0,0,0,0,1], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+rsub_Instr.setCode(opCode,'execute')
+isa.addInstruction(rsub_Instr)
+
+# RSUBC
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rsubc_Instr = trap.Instruction('RSUBC', True)
+rsubc_Instr.setMachineCode(oper_reg, {'opcode0': [0,0,0,0,1,1], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+rsubc_Instr.setCode(opCode,'execute')
+isa.addInstruction(rsubc_Instr)
+
+# RSUBK
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rsubk_Instr = trap.Instruction('RSUBK', True)
+rsubk_Instr.setMachineCode(oper_reg, {'opcode0': [0,0,0,1,0,1], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+rsubk_Instr.setCode(opCode,'execute')
+isa.addInstruction(rsubk_Instr)
+
+# RSUBKC
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rsubkc_Instr = trap.Instruction('RSUBKC', True)
+rsubkc_Instr.setMachineCode(oper_reg, {'opcode0': [0,0,0,1,1,1], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+rsubkc_Instr.setCode(opCode,'execute')
+isa.addInstruction(rsubkc_Instr)
+
+#RSUBI instruction family
+#RSUBI
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rsubi_Instr = trap.Instruction('RSUBI', True)
+rsubi_Instr.setMachineCode(oper_imm, {'opcode': [0,0,1,0,0,1]}, 'TODO')
+rsubi_Instr.setCode(opCode,'execute')
+isa.addInstruction(rsubi_Instr)
+
+#RSUBIC
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rsubic_Instr = trap.Instruction('RSUBIC', True)
+rsubic_Instr.setMachineCode(oper_imm, {'opcode': [0,0,1,0,1,1]}, 'TODO')
+rsubic_Instr.setCode(opCode,'execute')
+isa.addInstruction(rsubic_Instr)
+
+#RSUBIK
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rsubik_Instr = trap.Instruction('RSUBIK', True)
+rsubik_Instr.setMachineCode(oper_imm, {'opcode': [0,0,1,1,0,1]}, 'TODO')
+rsubik_Instr.setCode(opCode,'execute')
+isa.addInstruction(rsubik_Instr)
+
+#RSUBIKC
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rsubikc_Instr = trap.Instruction('RSUBIKC', True)
+rsubikc_Instr.setMachineCode(oper_imm, {'opcode': [0,0,1,1,1,1]}, 'TODO')
+rsubikc_Instr.setCode(opCode,'execute')
+isa.addInstruction(rsubikc_Instr)
+
+#RETURN instruction family
+#RTBD
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rtbd_Instr = trap.Instruction('RTBD','True')
+rtbd_Instr.setMachineCode(branch_cond_imm, {'opcode0': [1,0,1,1,0,1], 'opcode1': [1,0,0,1,0]}, 'TODO')
+rtbd_Instr.setCode(opCode,'execute')
+isa.addInstruction(rtbd_Instr)
+
+#RTID
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rtid_Instr = trap.Instruction('RTID','True')
+rtid_Instr.setMachineCode(branch_cond_imm, {'opcode0': [1,0,1,1,0,1], 'opcode1': [1,0,0,0,1]}, 'TODO')
+rtid_Instr.setCode(opCode,'execute')
+isa.addInstruction(rtid_Instr)
+
+#RTED
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rted_Instr = trap.Instruction('RTED','True')
+rted_Instr.setMachineCode(branch_cond_imm, {'opcode0': [1,0,1,1,0,1], 'opcode1': [1,0,1,0,0]}, 'TODO')
+rted_Instr.setCode(opCode,'execute')
+isa.addInstruction(rted_Instr)
+
+#RTSD
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+rtsd_Instr = trap.Instruction('RTSD','True')
+rtsd_Instr.setMachineCode(branch_cond_imm, {'opcode0': [1,0,1,1,0,1], 'opcode1': [1,0,0,0,0]}, 'TODO')
+rtsd_Instr.setCode(opCode,'execute')
+isa.addInstruction(rtsd_Instr)
+
+#STORE instruction family
+#SB
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+sb_Instr = trap.Instruction('SB', True)
+sb_Instr.setMachineCode(oper_reg, {'opcode0': [1,1,0,1,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+sb_Instr.setCode(opCode,'execute')
+isa.addInstruction(sb_Instr)
+
+#SBI
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+sbi_Instr = trap.Instruction('SBI', True)
+sbi_Instr.setMachineCode(oper_imm, {'opcode': [1,1,1,1,0,0]}, 'TODO')
+sbi_Instr.setCode(opCode,'execute')
+isa.addInstruction(sbi_Instr)
+
+#SH
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+sh_Instr = trap.Instruction('SH', True)
+sh_Instr.setMachineCode(oper_reg, {'opcode0': [1,1,0,1,0,1], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+sh_Instr.setCode(opCode,'execute')
+isa.addInstruction(sh_Instr)
+
+#SHI
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+shi_Instr = trap.Instruction('SHI', True)
+shi_Instr.setMachineCode(oper_imm, {'opcode': [1,1,1,1,0,1]}, 'TODO')
+shi_Instr.setCode(opCode,'execute')
+isa.addInstruction(shi_Instr)
+
+#SW
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+sw_Instr = trap.Instruction('SW', True)
+sw_Instr.setMachineCode(oper_reg, {'opcode0': [1,1,0,1,1,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+sw_Instr.setCode(opCode,'execute')
+isa.addInstruction(sw_Instr)
+
+#SWI
+opCode = cxx_writer.writer_code.Code("""
+T
+""")
+swi_Instr = trap.Instruction('SWI', True)
+swi_Instr.setMachineCode(oper_imm, {'opcode': [1,1,1,1,1,0]}, 'TODO')
+swi_Instr.setCode(opCode,'execute')
+isa.addInstruction(swi_Instr)
+
+#SEXT16
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+sext16_Instr = trap.Instruction('SEXT16', True)
+sext16_Instr.setMachineCode(unary_oper, {'opcode0': [1,0,0,1,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1]}, 'TODO')
+sext16_Instr.setCode(opCode,'execute')
+isa.addInstruction(sext16_Instr)
+
+#SEXT8
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+sext8_Instr = trap.Instruction('SEXT8', True)
+sext8_Instr.setMachineCode(unary_oper, {'opcode0': [1,0,0,1,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0]}, 'TODO')
+sext8_Instr.setCode(opCode,'execute')
+isa.addInstruction(sext8_Instr)
+
+#SRA
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+sra_Instr = trap.Instruction('SRA', True)
+sra_Instr.setMachineCode(unary_oper, {'opcode0': [1,0,0,1,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]}, 'TODO')
+sra_Instr.setCode(opCode,'execute')
+isa.addInstruction(sra_Instr)
+
+#SRC
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+src_Instr = trap.Instruction('SRC', True)
+src_Instr.setMachineCode(unary_oper, {'opcode0': [1,0,0,1,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1]}, 'TODO')
+src_Instr.setCode(opCode,'execute')
+isa.addInstruction(src_Instr)
+
+#SRL
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+srl_Instr = trap.Instruction('SRL', True)
+srl_Instr.setMachineCode(unary_oper, {'opcode0': [1,0,0,1,0,0], 'opcode1': [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1]}, 'TODO')
+srl_Instr.setCode(opCode,'execute')
+isa.addInstruction(srl_Instr)
+
+#WDC
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+wdc_Instr = trap.Instruction('WDC', True)
+wdc_Instr.setMachineCode(cache_oper, {'opcode0': [1,0,0,1,0,0], 'opcode1': [0,0,0,0,1,1,0,0,1,0,0]}, 'TODO')
+wdc_Instr.setCode(opCode,'execute')
+isa.addInstruction(wdc_Instr)
+
+#WIC
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+wic_Instr = trap.Instruction('WIC', True)
+wic_Instr.setMachineCode(cache_oper, {'opcode0': [1,0,0,1,0,0], 'opcode1': [0,0,0,0,1,1,0,1,0,0,0]}, 'TODO')
+wic_Instr.setCode(opCode,'execute')
+isa.addInstruction(wic_Instr)
+
+#XOR
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+xor_Instr = trap.Instruction('XOR', True)
+xor_Instr.setMachineCode(oper_reg, {'opcode0': [1,0,0,0,1,0], 'opcode1': [0,0,0,0,0,0,0,0,0,0,0]}, 'TODO')
+xor_Instr.setCode(opCode,'execute')
+isa.addInstruction(xor_Instr)
+
+#XORI
+opCode = cxx_writer.writer_code.Code("""
+
+""")
+xori_Instr = trap.Instruction('XORI', True)
+xori_Instr.setMachineCode(oper_imm, {'opcode': [1,0,1,0,1,0]}, 'TODO')
+xori_Instr.setCode(opCode,'execute')
+isa.addInstruction(xori_Instr)
