30db5cd2419f4bd1b42c8b8d42640f3ba643ea91
fossati.l <fossati.l@b5a49c6b-0751-0410-b22d-d588f1dd31d4>
2009-08-25 15:14:54 +0000

    Improved the script for the generation of the speed diagrams after benchmark execution

--- benchmarks/speedGraph.py
+++ benchmarks/speedGraph.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     for row in fileCsvReader:
         graphPoints.append((row[0].replace('_', '\_'), float(row[-2]), float(row[-1])))
 
-    g_cat = graph.graphxy(width=8, x=myaxis, y=graph.axis.linear(title="Execution Speed [MIPS]",painter=graph.axis.painter.regular(gridattrs=[style.linestyle.dashed,style.linewidth.Thin,color.gray(0.5)])))
+    g_cat = graph.graphxy(width=8, x=myaxis, y=graph.axis.linear(min=0, title="Execution Speed [MIPS]",painter=graph.axis.painter.regular(gridattrs=[style.linestyle.dashed,style.linewidth.Thin,color.gray(0.5)])))
     g_cat.plot([graph.data.points(graphPoints, xname=0, addlinenumbers = 0, y=1, dy=2)], styles=chosen_style)
 
     g_cat.writePDFfile(os.path.splitext(os.path.basename(sys.argv[1]))[0])
