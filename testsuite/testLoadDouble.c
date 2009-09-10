c69fcee5dc2bc96d96531bce1f9744ccfaeb5fff
fossati.l <fossati.l@b5a49c6b-0751-0410-b22d-d588f1dd31d4>
2009-09-10 06:43:12 +0000

    Corrected one test of the testsuite because of a problem with little endian targets

--- testsuite/testLoadDouble.c
+++ testsuite/testLoadDouble.c
@@ -3,7 +3,11 @@
 extern void _exit(int);
 
 int main(){
+    #ifndef BIG_TARGET
+    unsigned int a[] = {0x9abcdeff, 0x12345678};
+    #else
     unsigned int a[] = {0x12345678, 0x9abcdeff};
+    #endif
     if(*((unsigned long long *)&a) != 0x123456789abcdeffLL){
         _exit(-1);
     }
