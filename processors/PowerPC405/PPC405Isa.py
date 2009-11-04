263c44a424b1ff282cf46a2ba86cf34da8760ece
redu0007 <redu0007@b5a49c6b-0751-0410-b22d-d588f1dd31d4>
2009-11-04 23:24:43 +0000

    Updating files for PowerPC405 processor simulation using TRAP

--- processors/PowerPC405/PPC405Isa.py
+++ processors/PowerPC405/PPC405Isa.py
@@ -29,11 +29,10 @@
 #   Free Software Foundation, Inc.,
 #   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
 #   or see <http://www.gnu.org/licenses/>.
-#
-#   (c) Luca Fossati, fossati@elet.polimi.it
-#
+#   (a) Luca Fossati, fossati@elet.polimi.it
+#   (b) Redwanur Rahman, md.rahman@mail.polimi.it
+#   (c) Ashanka das, Ak.das@mail.polimi.it
 ####################################################################################
-
 
 
 # Lets first of all import the necessary files for the
@@ -87,7 +86,6 @@ adde_Instr.setCode(opCode,'execute')
 adde_Instr.addBehavior(IncrementPC, 'execute')
 adde_Instr.addTest({'rt': 3, 'ra': 1, 'rb': 2}, {'GPR[1]': 4, 'GPR[2]': 6, 'GPR[3]': 0xfffff, 'PC':0x0, 'GPR[4]':0x00000000, 'GPR[5]':0xffffffff}, {'GPR[3]': 10, 'PC':0x4, 'GPR[4]':0x00000000})
 isa.addInstruction(adde_Instr)
-
 #ADDI
 opCode = cxx_writer.writer_code.Code("""
 if(ra == 0){
@@ -103,4 +101,129 @@ addi_Instr.setCode(opCode,'execute')
 addi_Instr.addBehavior(IncrementPC, 'execute')
 #addi_Instr.addTest({'rt': 3, 'ra': 1}, {'GPR[1]': 4, 'GPR[2]': 6, 'GPR[3]': 0xfffff, 'PC':0x0, 'GPR[4]':0x00000000, 'GPR[5]':0xffffffff}, {'GPR[3]': 10, 'PC':0x4, 'GPR[4]':0x00000000})
 isa.addInstruction(addi_Instr)
+#ADDIC 
+#ADDIS
+#ADDME
+opCode = cxx_writer.writer_code.Code("""
+rt = (int)ra + XER[CA]+(-1);
+if ((int)ra + XER[CA]+0xFFFFFFFF > exp(2,32)-1) {
+	XER[CA] = 1;
+}
+else {
+	XER[CA] = 0;
+}
+""")
+addme_Instr = trap.Instruction('ADDME', True)
+addme_Instr.setMachineCode(oper_X0form_3, {'primary_opcode': [0,1,1,1,1,1], 'xo': [0,1,1,1,0,1,0,1,0]}, ('Add r', '%rt', ' r', '%ra'))
+addme_Instr.setCode(opCode,'execute')
+addme_Instr.addBehavior(IncrementPC, 'execute')
+#addme_Instr.addTest({'rt': 1, 'ra': 2}, {'GPR[1]': 4, 'GPR[2]': 6, 'GPR[3]': 0xfffff, 'PC':0x0, 'GPR[4]':0x00000000, 'GPR[5]':0xffffffff}, {'GPR[3]': 10, 'PC':0x4, 'GPR[4]':0x00000000})
+isa.addInstruction(addme_Instr)
+#ADDZE
+opCode = cxx_writer.writer_code.Code("""
+rt = (int)ra + XER[CA];
+if ((int)ra + XER[CA]> exp(2,32)-1) {
+	XER[CA] = 1;
+}
+else {
+	XER[CA] = 0;
+}
+""")
+addze_Instr = trap.Instruction('ADDZE', True)
+addze_Instr.setMachineCode(oper_X0form_3, {'primary_opcode': [0,1,1,1,1,1], 'xo': [0,0,0,0,1,1,1,0,0]}, ('Add r', '%rt', ' r', '%ra'))
+addze_Instr.setCode(opCode,'execute')
+addze_Instr.addBehavior(IncrementPC, 'execute')
+#addze_Instr.addTest({'rt': 1, 'ra': 2}, {'GPR[1]': 4, 'GPR[2]': 6, 'GPR[3]': 0xfffff, 'PC':0x0, 'GPR[4]':0x00000000, 'GPR[5]':0xffffffff}, {'GPR[3]': 10, 'PC':0x4, 'GPR[4]':0x00000000})
+isa.addInstruction(addze_Instr)
+#AND
+opCode = cxx_writer.writer_code.Code("""
+ra = (int)rs & (int)rb;
+""")
+and_Instr = trap.Instruction('AND', True)
+and_Instr.setMachineCode(oper_Xform_7, {'primary_opcode': [0,1,1,1,1,1], 'xo': [0,0,1,1,0,0,1,0,1,0]}, ('and r', '%ra', ' r', '%rs', ' r', '%rb'))
+and_Instr.setCode(opCode,'execute')
+and_Instr.addBehavior(IncrementPC, 'execute')
+and_Instr.addTest({'ra': 3, 'rs': 1, 'rb': 2}, {'GPR[1]': 0xffcc8844, 'GPR[2]': 0x66666666, 'GPR[3]': 0xfffff, 'PC':0x0, 'TARGET':0xffffffff}, {'GPR[3]': 0x66440044, 'PC':0x4})
+and_Instr.addTest({'ra': 1, 'rs': 1, 'rb': 1}, {'GPR[1]': 0xab88cd77, 'PC':0x0, 'TARGET':0xffffffff}, {'GPR[1]': 0xab88cd77, 'PC':0x4})
+isa.addInstruction(and_Instr)
+#ANDC
+opCode = cxx_writer.writer_code.Code("""
+ra = (int)rs & ~((int)rb);
+""")
+andc_Instr = trap.Instruction('ANDC', True)
+andc_Instr.setMachineCode(oper_Xform_7, {'primary_opcode': [0,1,1,1,1,1], 'xo': [0,0,0,0,1,1,1,1,0,0]}, ('and r', '%ra', ' r', '%rs', ' r', '%rb'))
+andc_Instr.setCode(opCode,'execute')
+andc_Instr.addBehavior(IncrementPC, 'execute')
+andc_Instr.addTest({'ra': 3, 'rs': 1, 'rb': 2}, {'GPR[1]': 0xffcc8844, 'GPR[2]': 0x66666666, 'GPR[3]': 0xfffff, 'PC':0x0, 'TARGET':0xffffffff}, {'GPR[3]': 0x99888800, 'PC':0x4})
+isa.addInstruction(andc_Instr)
+#ANDI 
+#ANDIS
+#B
+#BC
+#BCCTR
+#BCLR
+#CMP
+opCode = cxx_writer.writer_code.Code("""
+int c0,c1,c2,c3;
+if (int)ra < (int)rb c0=1;
+if (int)ra > (int)rb c1=1;
+if (int)ra = (int)rb c2=1;
+c3=XER[S0];
+int n=bf;
+CR[n]=c0; 
+//CR[1]=c1; CR[2]=c2; CR[3]=c3;
+""")
+cmp_Instr = trap.Instruction('CMP', True)
+cmp_Instr.setMachineCode(oper_Xform_16, {'primary_opcode': [0,1,1,1,1,1], 'xo': [0,0,0,0,0,0,0,0,0,0]}, ('cmp r', '%bf', ' r', '%ra', ' r', '%rb'))
+cmp_Instr.setCode(opCode,'execute')
+cmp_Instr.addBehavior(IncrementPC, 'execute')
+cmp_Instr.addTest({'bf': 3, 'ra': 1, 'rb': 2}, {'GPR[1]': 0xfd33, 'GPR[2]': 0xd6cc, 'GPR[3]': 0xfffff, 'PC':0x0, 'TARGET':0xffffffff}, {'GPR[3]': 0xffffd999, 'PC':0x4})
+isa.addInstruction(cmp_Instr)
+#CMPI
+opCode = cxx_writer.writer_code.Code("""
+int c0=0,c1=0,c2=0,c3=0;
+if (int)ra < exts(im_si) c0=1;
+if (int)ra > exts(im_si) c1=1;
+if (int)ra = exts(im_si) c2=1;
+c3=XER[S0];
+int n=bf;
+CR[n]=c0; 
+//CR[1]=c1; CR[2]=c2; CR[3]=c3;
+""")
+#cmpi_Instr = trap.Instruction('CMPI', True)
+#cmpi_Instr.setMachineCode(oper_Dform_5, {'primary_opcode': [0,0,1,1,0,1]}, ('cmpi r', '%bf', ' r', '%ra', ' r', '%rb'))
+#cmpi_Instr.setCode(opCode,'execute')
+#cmpi_Instr.addBehavior(IncrementPC, 'execute')
+#cmpi_Instr.addTest({'bf': 3, 'ra': 1, 'rb': 2}, {'GPR[1]': 0xfd33, 'GPR[2]': 0xd6cc, 'GPR[3]': 0xfffff, 'PC':0x0, 'TARGET':0xffffffff}, {'GPR[3]': 0xffffd999, 'PC':0x4})
+#isa.addInstruction(cmpi_Instr)
+#CMPL
+opCode = cxx_writer.writer_code.Code("""
+int c0,c1,c2,c3;
+if (int)ra < (int)rb c0=1;
+if (int)ra > (int)rb c1=1;
+if (int)ra = (int)rb c2=1;
+c3=XER[S0];
+int n=bf;
+CR[n]=c0; 
+//CR[1]=c1; CR[2]=c2; CR[3]=c3;
+""")
+cmpl_Instr = trap.Instruction('CMPL', True)
+cmpl_Instr.setMachineCode(oper_Xform_16, {'primary_opcode': [0,1,1,1,1,1], 'xo': [0,0,0,0,1,0,0,0,0,0]}, ('cmpi r', '%bf', ' r', '%ra', ' r', '%rb'))
+cmpl_Instr.setCode(opCode,'execute')
+cmpl_Instr.addBehavior(IncrementPC, 'execute')
+#cmpi_Instr.addTest({'bf': 3, 'ra': 1, 'rb': 2}, {'GPR[1]': 0xfd33, 'GPR[2]': 0xd6cc, 'GPR[3]': 0xfffff, 'PC':0x0, 'TARGET':0xffffffff}, {'GPR[3]': 0xffffd999, 'PC':0x4})
+isa.addInstruction(cmpl_Instr)
+#CMPLI
+#CNTLZW
+#CRAND
+#CRANDC
+#CREQV
+#CRNAND
+#CRNOR
+#CROR
+#CRORC
+#CRXOR
+#DCBA
+#DCBF
+#DCBI
 
