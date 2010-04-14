fef41245a0f176285373e1b89746985ba24301dd
fossati.l <fossati.l@b5a49c6b-0751-0410-b22d-d588f1dd31d4>
2010-04-14 17:52:41 +0000

    Corrected some bugs in the cross-compiler creation scripts

--- BSP_and_Compiler/canadianCrossCreate.py
+++ BSP_and_Compiler/canadianCrossCreate.py
@@ -86,110 +86,110 @@ except:
 binutilsName = ''
 if binutils.find('.tar.bz2') == len(binutils) - 8:
     os.system('tar -xjkf ' + binutils + ' 2> /dev/null')
-    binutilsName = binutils[:-8]
+    binutilsName = os.path.basename(binutils)[:-8]
 elif binutils.find('.tar.gz') == len(binutils) - 7:
     os.system('tar -xzkf ' + binutils + ' 2> /dev/null')
-    binutilsName = binutils[:-7]
+    binutilsName = os.path.basename(binutils)[:-7]
 elif binutils.find('.tgz') == len(binutils) - 4:
     os.system('tar -xzkf ' + binutils + ' 2> /dev/null')
-    binutilsName = binutils[:-4]
+    binutilsName = os.path.basename(binutils)[:-4]
 else:
-    print binutils + ' is not a valid archive (use gzipped or bzipper tarred archives)'
+    print binutils + ' is not a valid archive (use gzipped or bzipped tarred archives)'
     sys.exit()
-if os.path.exists(binutils + '_build'):
-    shutil.rmtree(binutils + '_build')
-os.mkdir(binutils + '_build')
+if os.path.exists(os.path.basename(binutils) + '_build'):
+    shutil.rmtree(os.path.basename(binutils) + '_build')
+os.mkdir(os.path.basename(binutils) + '_build')
 
 gccName = ''
 if gcc.find('.tar.bz2') == len(gcc) - 8:
     os.system('tar -xjkf ' + gcc + ' 2> /dev/null')
-    gccName = gcc[:-8]
+    gccName = os.path.basename(gcc)[:-8]
 elif gcc.find('.tar.gz') == len(gcc) - 7:
     os.system('tar -xzkf ' + gcc + ' 2> /dev/null')
-    gccName = gcc[:-7]
+    gccName = os.path.basename(gcc)[:-7]
 elif gcc.find('.tgz') == len(gcc) - 4:
     os.system('tar -xzkf ' + gcc + ' 2> /dev/null')
-    gccName = gcc[:-4]
+    gccName = os.path.basename(gcc)[:-4]
 else:
     print gcc + ' is not a valid archive (use gzipped or bzipper tarred archives)'
     sys.exit()
-if os.path.exists(gcc + '_build'):
-    shutil.rmtree(gcc + '_build')
-os.mkdir(gcc + '_build')
+if os.path.exists(os.path.basename(gcc) + '_build'):
+    shutil.rmtree(os.path.basename(gcc) + '_build')
+os.mkdir(os.path.basename(gcc) + '_build')
 
 newlibName = ''
 if newlib.find('.tar.bz2') == len(newlib) - 8:
     os.system('tar -xjkf ' + newlib + ' 2> /dev/null')
-    newlibName = newlib[:-8]
+    newlibName = os.path.basename(newlib)[:-8]
 elif newlib.find('.tar.gz') == len(newlib) - 7:
     os.system('tar -xzkf ' + newlib + ' 2> /dev/null')
-    newlibName = newlib[:-7]
+    newlibName = os.path.basename(newlib)[:-7]
 elif newlib.find('.tgz') == len(newlib) - 4:
     os.system('tar -xzkf ' + newlib + ' 2> /dev/null')
-    newlibName = newlib[:-4]
+    newlibName = os.path.basename(newlib)[:-4]
 else:
     print newlib + ' is not a valid archive (use gzipped or bzipper tarred archives)'
     sys.exit()
-if os.path.exists(newlib + '_build'):
-    shutil.rmtree(newlib + '_build')
-os.mkdir(newlib + '_build')
+if os.path.exists(os.path.basename(newlib) + '_build'):
+    shutil.rmtree(os.path.basename(newlib) + '_build')
+os.mkdir(os.path.basename(newlib) + '_build')
 
 if os.path.exists(insight):
     insightName = ''
     if insight.find('.tar.bz2') == len(insight) - 8:
         os.system('tar -xjkf ' + insight + ' 2> /dev/null')
-        insightName = insight[:-8]
+        insightName = os.path.basename(insight)[:-8]
     elif insight.find('.tar.gz') == len(insight) - 7:
         os.system('tar -xzkf ' + insight + ' 2> /dev/null')
-        insightName = insight[:-7]
+        insightName = os.path.basename(insight)[:-7]
     elif insight.find('.tgz') == len(insight) - 4:
         os.system('tar -xzkf ' + insight + ' 2> /dev/null')
-        insightName = insight[:-4]
+        insightName = os.path.basename(insight)[:-4]
     else:
         print insight + ' is not a valid archive (use gzipped or bzipper tarred archives)'
         sys.exit()
-    if os.path.exists(insight + '_build'):
-        shutil.rmtree(insight + '_build')
-    os.mkdir(insight + '_build')
+    if os.path.exists(os.path.basename(insight) + '_build'):
+        shutil.rmtree(os.path.basename(insight) + '_build')
+    os.mkdir(os.path.basename(insight) + '_build')
 elif os.path.exists(gdb):
     gdbName = ''
     if gdb.find('.tar.bz2') == len(gdb) - 8:
         os.system('tar -xjkf ' + gdb + ' 2> /dev/null')
-        gdbName = gdb[:-8]
+        gdbName = os.path.basename(gdb)[:-8]
     elif gdb.find('.tar.gz') == len(gdb) - 7:
         os.system('tar -xzkf ' + gdb + ' 2> /dev/null')
-        gdbName = gdb[:-7]
+        gdbName = os.path.basename(gdb)[:-7]
     elif gdb.find('.tgz') == len(gdb) - 4:
         os.system('tar -xzkf ' + gdb + ' 2> /dev/null')
-        gdbName = gdb[:-4]
+        gdbName = os.path.basename(gdb)[:-4]
     else:
         print gdb + ' is not a valid archive (use gzipped or bzipper tarred archives)'
         sys.exit()
-    if os.path.exists(gdb + '_build'):
-        shutil.rmtree(gdb + '_build')
-    os.mkdir(gdb + '_build')
+    if os.path.exists(os.path.basename(gdb) + '_build'):
+        shutil.rmtree(os.path.basename(gdb) + '_build')
+    os.mkdir(os.path.basename(gdb) + '_build')
 
 #Ok, lets finally procede with the actual compilation
 print '\nCompiling binutils\n'
-if os.system('cd ' + os.path.abspath(binutils + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(binutilsName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib ' + addFlags + ' && make -j' + str(numProc) + ' && make install') != 0:
+if os.system('cd ' + os.path.abspath(os.path.basename(binutils) + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(binutilsName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib ' + addFlags + ' && make -j' + str(numProc) + ' && make install') != 0:
     sys.exit()
 print '\nCompiling gcc step-1\n'
-if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(gcc + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(gccName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib --with-newlib --with-tls --with-__thread --enable-languages=\'c,c++\' --with-headers=' + os.path.abspath(newlibName + '/newlib/libc/include') + ' --disable-__cxa_atexit --disable-__dso_handle ' + addFlags + ' && make all-gcc -j' + str(numProc) + ' && make install-gcc') != 0:
+if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(os.path.basename(gcc) + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(gccName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib --with-newlib --with-tls --with-__thread --enable-languages=\'c,c++\' --with-headers=' + os.path.abspath(newlibName + '/newlib/libc/include') + ' --disable-__cxa_atexit --disable-__dso_handle ' + addFlags + ' && make all-gcc -j' + str(numProc) + ' && make install-gcc') != 0:
     sys.exit()
 if newlibPatch.lower() == 'y':
-    raw_input('Please perform all the necessary modifications to the newlib library and press a key when ready to continue')
+    raw_input('Please perform all the necessary modifications to the newlib library in folder ' + os.path.abspath(os.path.basename(newlib) + '_build') + ' and press a key when ready to continue')
 print '\nCompiling newlib\n'
-if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(newlib + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(newlibName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib ' + addFlags + ' && make -j' + str(numProc) + ' && make install') != 0:
+if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(os.path.basename(newlib) + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(newlibName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib ' + addFlags + ' && make -j' + str(numProc) + ' && make install') != 0:
     sys.exit()
 print '\nCompiling gcc step-2\n'
-if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(gcc + '_build') + ' && make -j' + str(numProc) + ' && make install') != 0:
+if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(os.path.basename(gcc) + '_build') + ' && make -j' + str(numProc) + ' && make install') != 0:
     sys.exit()
 #Now it is time to see if we need to cross-compiler GDB
 print '\nCompiling debugger\n'
 if os.path.exists(insight):
-    if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(insight + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(insightName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib ' + addFlags + ' && make -j' + str(numProc) + ' && make install') != 0:
+    if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(os.path.basename(insight) + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(insightName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib ' + addFlags + ' && make -j' + str(numProc) + ' && make install') != 0:
         sys.exit()
 elif os.path.exists(gdb):
-    if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(gdb + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(gdbName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib ' + addFlags + ' && make -j' + str(numProc) + ' && make install') != 0:
+    if os.system('export PATH=' + os.path.abspath(nativeToolchain + '/bin') + ':$PATH && cd ' + os.path.abspath(os.path.basename(gdb) + '_build') + ' && CC=i686-mingw32-gcc CXX=i686-mingw32-g++ ' + os.path.abspath(gdbName + '/configure') + ' --host=i686-mingw32 --build=i686-pc-linux-gnu --target=' + targetArch + ' --prefix=' + os.path.abspath(prefix) + ' --enable-multilib ' + addFlags + ' && make -j' + str(numProc) + ' && make install') != 0:
         sys.exit()
 print '\n\n\nCross-Compiler correctly created in ' + os.path.abspath(prefix)
